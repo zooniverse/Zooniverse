fdd45dc447c9e23bf7770a74ad6478d16db7d8d3
Ed Paget <ed.paget@gmail.com>
2012-08-09 15:42:11 -0500

    Add login form to exports

--- index.js
+++ index.js
@@ -8,3 +8,4 @@ exports.ProxyFrame = require('./lib/proxy_frame');
 exports.User = require('./lib/models/user');
 exports.Subject = require('./lib/models/subject');
 exports.Group = require('./lib/models/group');
+exports.LoginForm = require('./lib/controllers/login_form');
