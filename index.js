917ab28487c0b43d2e49f7d823d965bc2aaa6868
Brian Carstensen <brian.carstensen@gmail.com>
2012-09-27 08:46:12 -0500

    Add Google analytics to index

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ exports.ProxyFrame = require('./lib/proxy_frame');
 exports.BarGraph = require('./lib/bar_graph');
 exports.AutoPopup = require('./lib/auto_popup');
 exports.ActiveHashLinks = require('./lib/active_hash_links');
+exports.googleAnalytics = require('./lib/google_analytics');
 
 exports.LoginForm = require('./lib/controllers/login_form');
 exports.BrowserCheck = require('./lib/controllers/browser_check');
