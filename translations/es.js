60ea8651befea0abe19e77418542a899f6aaa0de
Chris Snyder <bumishness@gmail.com>
2014-03-04 14:21:33 -0600

    Add Spanish

--- translations/es.js
+++ translations/es.js
@@ -3,7 +3,30 @@
   var es, _base;
 
   es = {
-    topBarHeading: 'Un proyecto de la Zooniverse'
+    topBarHeading: 'Un proyecto de Zooniverse',
+    signUpHeading: 'Regístrate con una nueva cuenta Zooniverse',
+    signInHeading: 'Inicia sesión con tu cuenta de Zooniverse',
+    signUp: 'Suscribirse',
+    signIn: 'Iniciar sesión',
+    signOut: 'Salir',
+    username: 'Nombre de Usuario',
+    password: 'Contraseña',
+    email: 'Email',
+    realName: 'Nombre Real',
+    whyRealName: 'Esto será usado para agradecer a los colaboradores, por ejempl  en las conversaciones o carteles. Si no quieres ser mencionado públicament  déjalo en blanco.',
+    noAccount: '¿No tienes una cuenta?',
+    agreeToPrivacyPolicy: 'Estoy de acuerdo con <a href="https://www.zoonivers  org/privacy" target="_blank"> y su política de privacidad </a>.',
+    betaPreference: 'Me gustaría recibir noticias con oportunidades para probar   proporcionar feedback en los proyectos inéditos de Zooniverse',
+    forgotPassword: '¿Olvidaste tu contraseña?',
+    badLogin: 'Nombre de usuario o contraseña incorrecta',
+    signInFailed: 'Error al iniciar sesión',
+    signInForProfile: 'Inicia sesión para ver tu perfil.',
+    footerHeading: 'Zooniverse es una colección de proyectos de ciencia ciudadan  basados en la web que utiliza el esfuerzo de voluntarios para ayudar a lo investigadores con la avalancha de datos a la que se enfrentan.',
+    privacyPolicy: 'Política de privacidad',
+    forkOnGitHub: 'Fuente y errores',
+    recents: 'Recientes',
+    favorites: 'Favoritos',
+    none: 'ninguno'
   };
 
   if (window.zooniverse == null) {
